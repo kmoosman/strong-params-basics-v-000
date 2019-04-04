11e1110abe73672b7ec24c3018b86f48b7e5a49f
Katie Moosman <moosman.katie@gmail.com>
2019-04-04 01:43:52 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,13 +16,13 @@ class PostsController < ApplicationController
   @post.save
   redirect_to post_path(@post)
 end
- 
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
-	 
+
 	def edit
 		  @post = Post.find(params[:id])
 	end
